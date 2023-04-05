4a073e40a082959efae4236d30d0cdf429478cc8
mightGuy <onyejemeemmanuel65@gmail.com>
2023-04-05 21:14:00 +0100

    Fixed Image build issue | Angel added footer section

--- src/components/footer/Logos.tsx
+++ src/components/footer/Logos.tsx
@@ -9,37 +9,37 @@ import Hackerrank from "../../assets/Hackerrank.png";
 export const Logos = [
   {
     id: 1,
-    logo: " Slack ",
+    logo: Slack,
     name: "Slack",
   },
   {
     id: 2,
-    logo: { KPMG },
+    logo: KPMG ,
     name: "KPMG",
   },
   {
     id: 3,
-    logo: { Workforce },
+    logo: Workforce ,
     name: "Workforce",
   },
   {
     id: 4,
-    logo: { Alx },
+    logo: Alx ,
     name: "Alx",
   },
   {
     id: 5,
-    logo: { Github },
+    logo: Github ,
     name: "Github",
   },
   {
     id: 6,
-    logo: { Holberton },
+    logo: Holberton,
     name: "Holberton",
   },
   {
     id: 7,
-    logo: { Hackerrank },
+    logo: Hackerrank,
     name: "Hackerrank",
   },
 ];
